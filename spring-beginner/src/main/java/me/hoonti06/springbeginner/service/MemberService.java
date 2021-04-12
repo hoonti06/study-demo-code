3c6e2747185eec85a6ce974ea799335f73927275
hoon <hoonti06@gmail.com>
2021-04-12 16:08:01 +0900

    method join() 내부에서 method chaining으로 변경

--- spring-beginner/src/main/java/me/hoonti06/springbeginner/service/MemberService.java
+++ spring-beginner/src/main/java/me/hoonti06/springbeginner/service/MemberService.java
@@ -15,10 +15,10 @@ public class MemberService {
      */
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
-            });
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
 
         memberRepository.save(member).getId();
         return member.getId();
